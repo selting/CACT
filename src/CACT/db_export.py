68d11ff8d9bab72513ce885d73cd26f683302eee
Steffen Elting <elting@cl.rwth-aachen.de>
2026-04-15 14:36:39 +0200

    small script to free up space in the mlflow DB

--- src/CACT/db_export.py
+++ src/CACT/db_export.py
@@ -11,7 +11,7 @@ parser.add_argument('--tag', nargs=1, type=str)
 def export_params_and_metrics(tag: str, out_dir: Path = Path('src/CACT/data/experiments/exports')):
     db_url = os.getenv('CACT_DB_URL')
     if db_url is None:
-        raise EnvironmentError("CACT_DB_URL environment variabel must be defined")
+        raise EnvironmentError("CACT_DB_URL environment variable must be defined")
     tag_filters = {"group_id": tag}
     params, metrics = parameters_and_metrics(db_url=db_url, tag_filters=tag_filters, param_filters=None)
     print(f'Getting data from {db_url} with tag {tag}')
---
+++ src/CACT/mlflow_delete_runs_by_tag.py
@@ -0,0 +1,73 @@
+import mlflow
+import subprocess
+import os
+import argparse
+from evaluation_module.db_functions import fetch_filtered_run_uuids
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--tag', nargs=1, type=str)
+parser.add_argument('--dryrun', action='store_true')
+def check_whether_exported(tag:str):
+    # check whether the runs have been exported to make sure that no accidental deletion happens
+    metrics_exists = os.path.exists(f"src/CACT/data/experiments/exports/['{tag}']-metrics.parquet")
+    if not metrics_exists:
+        print(f"No metrics export found for tag {tag}")
+        return False
+    params_exists = os.path.exists(f"src/CACT/data/experiments/exports/['{tag}']-params.parquet")
+    if not params_exists:
+        print(f"No params export found for tag {tag}")
+        return False
+    if metrics_exists and params_exists:
+        return True
+
+def delete_runs_by_tag(tag: str, dry_run: bool = False):
+    """
+    Set runs with the given tag to 'deleted' lifecycle stage
+    """
+    CACT_DB_URL = os.getenv('CACT_DB_URL')
+    if not CACT_DB_URL:
+        raise EnvironmentError("CACT_DB_URL environment variable must be defined")
+    MLFLOW_TRACKING_URI = os.getenv('MLFLOW_TRACKING_URI')
+    if not MLFLOW_TRACKING_URI:
+        raise EnvironmentError("MLFLOW_TRACKING_URI environment variable must be defined")
+
+    # Get all runs with the specified tag
+    run_uuids = fetch_filtered_run_uuids(db_url=CACT_DB_URL, tag_filters={"group_id": [tag]})
+
+    # Set them to status "deleted"
+    client = mlflow.MlflowClient(MLFLOW_TRACKING_URI)
+    for run_uuid in tqdm(run_uuids, desc=f"Deleting runs {'(dry-run)' if dry_run else ''}'):"):
+        if not dry_run:
+            client.delete_run(run_uuid, delete_artifacts=False)
+
+    # use mlflow CLI command 'mlflow gc' to permanently delete the runs from the database
+    if dry_run:
+        print("Dry run mode: No runs will be deleted from the database")
+    else:
+        result = subprocess.run(['mlflow', 'gc'], capture_output=True)
+        # Get the output of the command
+        output = result.stdout.decode()
+        # Print the output
+        print(output)
+
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    tag = args.tag[0]
+    dry_run = args.dryrun[0]
+    if dry_run:
+        print("DRY RUN")
+
+    exported = check_whether_exported(tag)
+
+    if not exported:
+        # ask user whether runs should be deleted anyway
+        print(f"Warning: No (full) export of runs with tag '{tag}' found. Are you sure you want to delete these runs? (y/n)")
+        if input().lower() != 'y':
+            print("Aborting deletion.")
+            exit(1)
+        else:
+            print("Proceeding with deletion.")
+            delete_runs_by_tag(tag)
