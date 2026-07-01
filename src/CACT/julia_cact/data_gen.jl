10bd8578a3698c7ab20fef1ff0cf30d5fc51fc62
Steffen Elting <elting@cl.rwth-aachen.de>
2026-07-01 18:17:51 +0200

    more flexible location generation (random, clustered)

--- src/CACT/julia_cact/data_gen.jl
+++ src/CACT/julia_cact/data_gen.jl
@@ -26,16 +26,39 @@ function draw_bundles(; rng, num_bundles::Int, auction_pool::Matrix)
     return bundles
 end
 
-function generate_input_data(; seed, x_min, x_max, y_min, y_max, true_num_locations::Int, size_auction_pool::Int, num_bundles::Int, pred_num_locations::Int)
+abstract type LocationGenerator end
+
+struct UniformGenerator <: LocationGenerator end
+
+struct ClusteredGenerator <: LocationGenerator
+    num_clusters::Int
+    cluster_std::Float64
+end
+
+# Common interface, dispatched on generator type
+function generate_locations(::UniformGenerator, rng, x_min, x_max, y_min, y_max, num_locations)
+    x = rand(rng, Uniform(x_min, x_max), num_locations)
+    y = rand(rng, Uniform(y_min, y_max), num_locations)
+    return [x y]
+end
+
+function generate_locations(gen::ClusteredGenerator, rng, x_min, x_max, y_min, y_max, num_locations)
+    center_x = rand(rng, Uniform(x_min, x_max), gen.num_clusters)
+    center_y = rand(rng, Uniform(y_min, y_max), gen.num_clusters)
+    assignments = rand(rng, 1:gen.num_clusters, num_locations)
+
+    # TODO implement rejection sampling instead of clamping?!
+    x = clamp.(center_x[assignments] .+ rand(rng, Normal(0, gen.cluster_std), num_locations), x_min, x_max)
+    y = clamp.(center_y[assignments] .+ rand(rng, Normal(0, gen.cluster_std), num_locations), y_min, y_max)
+    return [x y]
+end
+
+
+function generate_input_data(;generator::LocationGenerator, seed, x_min, x_max, y_min, y_max, true_num_locations::Int, size_auction_pool::Int, num_bundles::Int, pred_num_locations::Int)
     rng = MersenneTwister(seed)
 
-    true_base_locations_x = rand.(rng, Uniform.(x_min, x_max), true_num_locations)
-    true_base_locations_y = rand.(rng, Uniform.(y_min, y_max), true_num_locations)
-    true_base_locations = [true_base_locations_x true_base_locations_y]
-
-    auction_pool_x = rand.(rng, Uniform.(x_min, x_max), size_auction_pool)
-    auction_pool_y = rand.(rng, Uniform.(y_min, y_max), size_auction_pool)
-    auction_pool = [auction_pool_x auction_pool_y]
+    true_base_locations = generate_locations(generator, rng, x_min, x_max, y_min, y_max, true_num_locations)
+    auction_pool = generate_locations(UniformGenerator(), rng, x_min, x_max, y_min, y_max, size_auction_pool) 
 
     bundles = draw_bundles(rng=rng, num_bundles=num_bundles, auction_pool=auction_pool)
     true_carrier_bids = compute_bids(true_base_locations, bundles)
